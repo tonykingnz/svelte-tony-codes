03b310daa64dba2e43bddfaa6b069c5cac08da29
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-11-12 10:22:18 -0300

    Solved exercice with Italo

--- exec-03-password/src/App.svelte
+++ exec-03-password/src/App.svelte
@@ -1,7 +1,46 @@
+<!--	Bonus: If a password is clicked, remove it from the list. -->
 <script>
+    let password = '';
+    let passwords = [];
+    $: passwordLength = password.length; 
+	
+    function addPassword(){
+        passwords = [...passwords, password];
+        console.log(passwords);
+        console.log(password);
+        console.log(passwordLength);
+    }
+    function removePassword(index){
+        passwords = passwords.filter((password, idx) => {return idx !== index;});
+        console.log(idx);
+    }
 </script>
 
 <main>
+    <div>
+        <h1>Write your password</h1>
+        <input type="password" bind:value={password} />
+
+        {#if passwordLength < 5}
+            <p>Password to short, minimium of 5</p>
+        {:else if passwordLength > 10}
+            <p>Password to long, maxmium of 10</p>
+        {:else} 
+        
+        <button on:click={addPassword}>
+            Add a Password
+        </button>
+
+        <p>{password}</p>
+        {/if}
+        
+        <ul>
+        {#each passwords as password, i (password)}
+        <li on:click={removePassword.bind(this, i)}>{password}</li>
+        {/each}
+        </ul>
+    
+    </div>
 </main>
 
 <style>
---
+++ exec-03-password/src/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let funcToUse;
+    export let buttonText = "Template button text";
+</script>
+
+<main>
+    <button on:click={funcToUse}>
+        <p>{buttonText}</p>
+    </button>
+</main>
